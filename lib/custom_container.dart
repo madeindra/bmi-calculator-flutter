714f03c2bb734e46361db44596e186a0e2ea99c3
Made Indra <made.indra@protonmail.com>
2021-03-28 20:36:29 +0700

    add onTap contructor for gestureDetector

--- lib/custom_container.dart
+++ lib/custom_container.dart
@@ -4,20 +4,24 @@ final double marginSize = 15.0;
 final double borderRadius = 10.0;
 
 class CustomContainer extends StatelessWidget {
-  const CustomContainer({this.child, this.color});
+  const CustomContainer({this.child, this.color, this.onTap});
 
   final Widget child;
   final Color color;
+  final Function onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(marginSize),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(borderRadius),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(marginSize),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: child,
       ),
-      child: child,
     );
   }
 }
